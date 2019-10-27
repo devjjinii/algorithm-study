ceca5e2bf3d0efcddb84e716feb347c0efc54259
jjinii <53853730+devjjinii@users.noreply.github.com>
2019-10-27 18:44:21 +0900

    Reverse Integer 완료

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -17,12 +17,15 @@ Output: 21
 class Solution {
     public int reverse(int x) {
         int rev = 0;
+        //10 을 통한 나머지와 나누기
         while (x != 0) {
-            int pop = x % 10;
-            x /= 10;
+            int pop = x % 10; 
+            x /= 10;             
+            // Integer.MAX_VALUE : 32비트 최대 정수값을 나타냄( 2147483647)
             if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            // Integer.MIN_VALUE : 최소 정수값 ( -2147483648)
             if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
-            rev = rev * 10 + pop;
+            rev = rev * 10 + pop; 
         }
         return rev;
     }
