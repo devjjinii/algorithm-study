e88f6a111fce932fba081aa7bac248f1b2f8e26e
jjinii <53853730+devjjinii@users.noreply.github.com>
2019-11-03 15:23:56 +0900

    Create Roman to Integer.java

---
+++ Roman to Integer.java
@@ -0,0 +1,43 @@
+/*
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+Example 1:
+Input: "III"
+Output: 3
+
+Example 2:
+Input: "IV"
+Output: 4
+
+Example 3:
+Input: "IX"
+Output: 9
+
+Example 4:
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 5:
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+*/
+
+       // Map
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
