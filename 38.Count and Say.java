10351f29aaab38ea769fa6da7f2ad8b2828e7c10
jjinii <53853730+devjjinii@users.noreply.github.com>
2019-11-28 16:11:39 +0900

    Create 38.Count and Say.java

---
+++ 38.Count and Say.java
@@ -0,0 +1,47 @@
+public class CountandSay {
+	/*
+	 * 1.     1
+	   2.     11
+	   3.     21
+	   4.     1211
+	   5.     111221
+	1 is read off as "one 1" or 11.
+	11 is read off as "two 1s" or 21.
+	21 is read off as "one 2, then one 1" or 1211.
+	
+	Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+	 * 
+	 * */
+	public static void main(String[] args) {
+		
+		System.out.println(countAndSay(4));
+	}
+	
+	public static String countAndSay(int n) {
+		if (n <= 0) return null;
+		
+		String result = "1";
+		int i = 1;
+		
+		while(i < n) {
+			StringBuilder sb = new StringBuilder();
+			int count = 1;
+			for ( int j=1; j < result.length(); j++ ) {
+				if(result.charAt(j) == result.charAt(j-1)) {
+					count ++;
+				} else {
+					sb.append(count);
+					sb.append(result.charAt(j-1));
+					count = 1;
+				}
+				
+			}
+			sb.append(count);
+			sb.append(result.charAt(result.length()-1));
+			result = sb.toString();
+			i++;
+		
+		}
+		return result;
+	}
+}
