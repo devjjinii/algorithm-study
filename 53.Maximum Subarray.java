77030a81f45b67f7ec914274a3b6ff674d048184
jjinii <53853730+devjjinii@users.noreply.github.com>
2019-12-11 11:19:42 +0900

    Update 53.Maximum Subarray.java

--- 53.Maximum Subarray.java
+++ 53.Maximum Subarray.java
@@ -1,25 +1,57 @@
+package calendar;
+
 /*
-Example:
-
-Input: [-2,1,-3,4,-1,2,1,-5,4],
-Output: 6
-Explanation: [4,-1,2,1] has the largest sum = 6.
-
-다시 풀기 -> 정답 확인하기.
+	Example:
+	Input: [-2,1,-3,4,-1,2,1,-5,4],
+	Output: 6
+	
+	https://www.youtube.com/watch?v=2MmGzdiKR9Y 설명 짱이다 :)
+	참조 : https://github.com/Blankj/awesome-java-leetcode 
+	
 */
 
+public class MaximumSubarray {
 
-class Solution {
-    public int maxSubArray(int[] nums) {
-        if(n<=0) return 0;
-       
-        int maxSum = A[0];
-        int curSum = A[0];
-        
-        for(int i=1; i<n; i++) {
-            curSum =  curSum<=0 ? A[i] : A[i]+curSum;
-            maxSum = max(maxSum , curSum);
-        }
-        return maxSum;
+	
+	public static void main(String[] args) {
+		 int arr[] = {4, -1, 2, 1}; 
+	     int n = arr.length; 
+	     System.out.println(maxSubArray(arr));
+	
+	}
+	
+	public static int maxSubArray(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
     }
+
+	private static int helper(int[] nums, int left, int right) {
+		
+		// Explanation: [4,-1,2,1] has the largest sum = 6.
+		// 배열에서 가장 큰 연속된 부분배열 합
+		
+		if(left >= right) {
+			return nums[left];
+		}
+		
+		int mid = (left + right) >> 1;
+		int leftAns = helper(nums, left, mid);
+		int rigthAns = helper(nums, mid+1, right);
+		int leftMax = nums[mid];
+		int rightMax = nums[mid + 1];
+		int temp = 0;
+		
+		for(int i=mid; i>=left; --i) {
+			temp += nums[i];
+			if(temp > leftMax) leftMax = temp;
+		}
+		temp = 0;
+		
+		for(int i=mid+1; i<=right; ++i) {
+			temp += nums[i];
+			if(temp > rightMax) rightMax = temp;
+		}
+		return Math.max(Math.max(leftAns, rigthAns), leftMax + rightMax);
+		
+	}
 }
+
