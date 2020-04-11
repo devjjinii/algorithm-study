86b4760687c4739d8e9b95495192f4cad361b4ff
devjjinii <cyujin11010@naver.com>
2020-04-11 18:11:06 +0900

    67,69 update

---
+++ algorithm/_069/SqrtX.java
@@ -0,0 +1,34 @@
+package _069;
+
+/*
+*   Example 1:
+	Input: 4
+	Output: 2
+
+	Example 2:
+	Input: 8
+	Output: 2
+	Explanation: The square root of 8 is 2.82842..., and since 
+	             the decimal part is truncated, 2 is returned. 
+ * */
+
+public class SqrtX {
+
+	public static void main(String[] args) {
+		System.out.println(mySqrt(8));
+
+	}
+	 public static int mySqrt(int x) {
+		  
+//		  long n = x;
+//		  while (n * n > x) {
+//			  n = (n + x / n) >> 1;
+//		  }
+//		  
+//		  return (int) n;
+		  
+		  double result = Math.sqrt(x);
+		  return (int) result;
+	  }
+
+}
