befacf211b817de6becdf67d82205e3622a23a6e
YuJin Choi <53853730+devjjinii@users.noreply.github.com>
2020-12-01 09:31:02 +0900

    2점..

---
+++ programmers/09. 서울에서 김서방 찾기.js
@@ -0,0 +1,14 @@
+function solution(seoul) {
+    // 인덱스 위치를 찾음
+    for(let i=0; i<seoul.length; i++) {
+        // console.log(seoul[i])
+        if(seoul[i] === 'Kim') {
+           return `김서방은 ${i}에 있다.`;
+        }
+    }
+}
+
+ // string 형 배열 seoul
+console.log(solution(['Jane', 'Kim']))
+console.log(solution(['Kim','Jane']))
+console.log(solution(['Jane','Dev', 'Hi','Kim']))
