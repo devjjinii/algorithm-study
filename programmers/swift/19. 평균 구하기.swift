076da3e3287ac432fce4b63984e0f526285f368a
YuJin Choi <53853730+devjjinii@users.noreply.github.com>
2021-02-03 09:33:39 +0900

    Create 19. 평균 구하기.swift

---
+++ programmers/swift/19. 평균 구하기.swift
@@ -0,0 +1,11 @@
+func solution(_ arr:[Int]) -> Double {
+    var sum: Int = 0
+    
+    for i in arr {
+        sum += i
+    }
+    
+    return Double(sum) / Double(arr.count)
+    
+    // return Double(arr.reduce(0,+))/Double(arr.count)
+}
